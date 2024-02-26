7e293412f5f44f6cabcb4f9f77fc171596fe787a
Kotaro Iwanaga <kotakota0313@gmail.com>
2024-02-25 16:03:11 -0800

    complete Kotaro's about page

--- application/client/src/AboutMembers/AboutKotaro.js
+++ application/client/src/AboutMembers/AboutKotaro.js
@@ -1,27 +1,29 @@
 // about page for Kotaro
 
-import React from 'react'
-import '../Stylesheets/App.css'
-import '../Stylesheets/About.css'
+import React from "react";
+import "../Stylesheets/App.css";
+import "../Stylesheets/About.css";
 // import *Image from ./images/MemberPics/*.png
+import KotaroImage from "../images/MemberPics/Kotaro.jpg";
 
-function AboutKotaro(){
-    return (
-        <div className="App"> 
-            <h1 className='title'>About FIRSTNAME LASTNAME</h1>
-            <div className="about-me-description-container">
-                <div className="about-me-image-container">
-                    <img className="about-me-image"
-                        src={'x'} alt='placeholder' />
-                    <p>Github: x</p>
-                </div>
-                <p className="about-me-description">
-                    DESCRIPTION HERE lorem ipsum dolor sit amet, consectetur adipiscing elit.DESCRIPTION HERE lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    DESCRIPTION HERE lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-            </div>
+function AboutKotaro() {
+  return (
+    <div className="App">
+      <h1 className="title">About Kotaro Iwanaga</h1>
+      <div className="about-me-description-container">
+        <div className="about-me-image-container">
+          <img className="about-me-image" src={KotaroImage} alt="placeholder" />
+          <p>Github: iamkotaaax</p>
         </div>
-    )
+        <p className="about-me-description">
+          Hello! My name is Kotaro Iwanaga. I am the database admin for the
+          team. I love to watch soccer and my favorite team is Real Madrid. What
+          is your favorite team? If you like to play soccer, join us on TeamUp
+          and let's play soccer together!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutKotaro;
--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,46 +1,76 @@
-import {Routes, Route} from 'react-router-dom';
-import './Stylesheets/App.css'
-import TeamMemberList from './TeamMemberList'
-import NavigationBar from './NavigationBar';
+import { Routes, Route } from "react-router-dom";
+import "./Stylesheets/App.css";
+import TeamMemberList from "./TeamMemberList";
+import NavigationBar from "./NavigationBar";
 
 // import *Image from ./images/MemberPics/*.png
-import JayceeImage from './images/MemberPics/Jaycee.png';
-import MartinImage from './images/MemberPics/Martin.png';
-import ColeImage from './images/MemberPics/Cole.png';
+import JayceeImage from "./images/MemberPics/Jaycee.png";
+import MartinImage from "./images/MemberPics/Martin.png";
+import ColeImage from "./images/MemberPics/Cole.png";
+import KotaroImage from "./images/MemberPics/Kotaro.jpg";
 
-import AboutJuan from './AboutMembers/AboutJuan';
-import AboutAreeb from './AboutMembers/AboutAreeb';
-import AboutMartin from './AboutMembers/AboutMartin';
-import AboutCole from './AboutMembers/AboutCole';
-import AboutJaycee from './AboutMembers/AboutJaycee';
-import AboutKotaro from './AboutMembers/AboutKotaro';
-
+import AboutJuan from "./AboutMembers/AboutJuan";
+import AboutAreeb from "./AboutMembers/AboutAreeb";
+import AboutMartin from "./AboutMembers/AboutMartin";
+import AboutCole from "./AboutMembers/AboutCole";
+import AboutJaycee from "./AboutMembers/AboutJaycee";
+import AboutKotaro from "./AboutMembers/AboutKotaro";
 
 const teamMembers = [
-  {name: 'Juan Estrada', imageUrl: '', role: 'Team Lead'},
-  {name: 'Areeb Abbasi', imageUrl: '', role: 'Backend Lead'},
-  {name: 'Martin Pham', imageUrl: MartinImage, role: 'Github Master'},
-  {name: 'Cole Chiodo', imageUrl: ColeImage, role: 'Docs Editor'},
-  {name: 'Kotaro Iwanaga', imageUrl: '', role: 'Database Admin'},
-  {name: 'Jaycee Lorenzo', imageUrl: JayceeImage, role: 'Frontend Lead'}
-]
+  { name: "Juan Estrada", imageUrl: "", role: "Team Lead" },
+  { name: "Areeb Abbasi", imageUrl: "", role: "Backend Lead" },
+  { name: "Martin Pham", imageUrl: MartinImage, role: "Github Master" },
+  { name: "Cole Chiodo", imageUrl: ColeImage, role: "Docs Editor" },
+  { name: "Kotaro Iwanaga", imageUrl: KotaroImage, role: "Database Admin" },
+  { name: "Jaycee Lorenzo", imageUrl: JayceeImage, role: "Frontend Lead" },
+];
 
 function App() {
   return (
     <div className="App">
-        <NavigationBar />
-          <Routes>
-            <Route path='/' element={<TeamMemberList teamMembers={teamMembers} />} />
-            <Route path='/about/:name' element={<TeamMemberList teamMembers={teamMembers} />} />
-            <Route path="about/juan" element={<h1>Juan</h1>} Component={AboutJuan} />
-            <Route path="about/areeb" element={<h1>Areeb</h1>} Component={AboutAreeb} />
-            <Route path="about/martin" element={<h1>Martin</h1>} Component={AboutMartin} />
-            <Route path="about/cole" element={<h1>Cole</h1>} Component={AboutCole} />
-            <Route path="about/kotaro" element={<h1>Kotaro</h1>} Component={AboutKotaro}/>
-            <Route path="about/jaycee" element={<h1>Jaycee</h1>} Component={AboutJaycee}/>
-          </Routes>
+      <NavigationBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<TeamMemberList teamMembers={teamMembers} />}
+        />
+        <Route
+          path="/about/:name"
+          element={<TeamMemberList teamMembers={teamMembers} />}
+        />
+        <Route
+          path="about/juan"
+          element={<h1>Juan</h1>}
+          Component={AboutJuan}
+        />
+        <Route
+          path="about/areeb"
+          element={<h1>Areeb</h1>}
+          Component={AboutAreeb}
+        />
+        <Route
+          path="about/martin"
+          element={<h1>Martin</h1>}
+          Component={AboutMartin}
+        />
+        <Route
+          path="about/cole"
+          element={<h1>Cole</h1>}
+          Component={AboutCole}
+        />
+        <Route
+          path="about/kotaro"
+          element={<h1>Kotaro</h1>}
+          Component={AboutKotaro}
+        />
+        <Route
+          path="about/jaycee"
+          element={<h1>Jaycee</h1>}
+          Component={AboutJaycee}
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
