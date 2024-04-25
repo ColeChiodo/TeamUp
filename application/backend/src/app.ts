5467b123b7da6a8c9faa3025715a6e4552da9f8f
areeeeb <ouziester@gmail.com>
2024-04-25 02:15:51 -0700

    added cors policy

--- application/backend/src/app.ts
+++ application/backend/src/app.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import helmet from 'helmet';
-import compression from 'compression';
-import cors from 'cors';
-import passport from 'passport';
-import httpStatus from 'http-status';
-import config from './config/config';
-import morgan from './config/morgan';
-import xss from './middlewares/xss';
-import { jwtStrategy } from './config/passport';
-import { authLimiter } from './middlewares/rateLimiter';
-import routes from './routes/v1';
-import { errorConverter, errorHandler } from './middlewares/error';
-import ApiError from './utils/ApiError';
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import passport from "passport";
+import httpStatus from "http-status";
+import config from "./config/config";
+import morgan from "./config/morgan";
+import xss from "./middlewares/xss";
+import { jwtStrategy } from "./config/passport";
+import { authLimiter } from "./middlewares/rateLimiter";
+import routes from "./routes/v1";
+import { errorConverter, errorHandler } from "./middlewares/error";
+import ApiError from "./utils/ApiError";
 
 const app = express();
 
-if (config.env !== 'test') {
+if (config.env !== "test") {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
@@ -36,27 +36,33 @@ app.use(xss());
 app.use(compression());
 
 // enable cors
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
+// Allow specific origins or use '*' for all origins (not recommended for production)
+app.use(
+  cors({
+    origin: "https://ft97cem3fc.us-east-1.awsapprunner.com",
+  })
+);
 
 // jwt authentication
 app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
+passport.use("jwt", jwtStrategy);
 
-console.log('the environment is: ');
+console.log("the environment is: ");
 console.log(config.env);
 
 // limit repeated failed requests to auth endpoints
-if (config.env === 'production') {  
-  app.use('/v1/auth', authLimiter);
+if (config.env === "production") {
+  app.use("/v1/auth", authLimiter);
 }
 
 // v1 api routes
-app.use('/v1', routes);
+app.use("/v1", routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
 
 // convert error to ApiError, if needed
